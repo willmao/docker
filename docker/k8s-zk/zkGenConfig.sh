20830b9af40f013ed7ead8996b04129208d62c89
willmao <maoguangqing@yit.com>
2018-10-13 12:10:36 +0800

    revet

--- docker/k8s-zk/zkGenConfig.sh
+++ docker/k8s-zk/zkGenConfig.sh
@@ -5,7 +5,7 @@
 ZK_CLIENT_PORT=${ZK_CLIENT_PORT:-2181}
 ZK_SERVER_PORT=${ZK_SERVER_PORT:-2888}
 ZK_ELECTION_PORT=${ZK_ELECTION_PORT:-3888}
-
+ZOO_DATA_DIR=${ZOO_DATA_DIR:-/data}
 HOST=`hostname -s`
 DOMAIN=`hostname -d`
 
@@ -33,12 +33,6 @@ fi
 
 MY_ID=$((ORD+1))
 
-# Allow the container to be started with `--user`
-if [[ "$1" = 'zkServer.sh' && "$(id -u)" = '0' ]]; then
-    chown -R "$ZOO_USER" "$ZOO_DATA_DIR" "$ZOO_DATA_LOG_DIR" "$ZOO_LOG_DIR" "$ZOO_CONF_DIR"
-    exec su-exec "$ZOO_USER" "$0" "$@"
-fi
-
 # Generate the config only if it doesn't exist
 if [[ ! -f "$ZOO_CONF_DIR/zoo.cfg" ]]; then
     CONFIG="$ZOO_CONF_DIR/zoo.cfg"
@@ -60,20 +54,7 @@ if [[ ! -f "$ZOO_CONF_DIR/zoo.cfg" ]]; then
     fi
 fi
 
-# Write myid only if it doesn't exist
-echo $MY_ID
-[[ -f "$ZOO_DATA_DIR/myid" ]] && cat "$ZOO_DATA_DIR/myid"
-if [[ ! -f "$ZOO_DATA_DIR/myid" ]]; then
-    echo "${MY_ID:1}" > "$ZOO_DATA_DIR/myid"
-fi
-
-# change own hostname to 0.0.0.0
-# otherwise, the own hostname will resolve to 127.0.0.1
-# https://stackoverflow.com/a/40750900/5764665
-# fullHostname="${HOST}.${DOMAIN}"
-# sed -i -e "s/${fullHostname}/0.0.0.0/g" "$CONFIG"
-
-# exec "$@"
-
+echo $MY_ID > "$ZOO_DATA_DIR/myid"
 cat "$CONFIG"
 cat "$ZOO_DATA_DIR/myid"
+# exec "$@"
